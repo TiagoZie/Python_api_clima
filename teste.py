5a47135ad082d17f29b984cc5a46408b12059d30
TiagoZie <tiagopodas@gmail.com>
2025-04-08 11:05:58 -0300

    removed useless code

--- teste.py
+++
@@ -1,7 +0,0 @@
-
-def test_groq_key():
-    headers = {"Authorization": f"Bearer {GROQ_API_KEY}"}
-    response = requests.get("https://api.groq.com/openai/v1/models", headers=headers)
-    print("Status do teste de chave:", response.status_code)
-    
-test_groq_key() 
